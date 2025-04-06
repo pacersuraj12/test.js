f1f4f613536bc23e80127e9c41e8271a55adb819
suraj tomar <140705055+pacersuraj12@users.noreply.github.com>
2025-04-06 16:15:38 +0000

    operations in conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,21 +1,19 @@
-/*let score = "suraj";
+let score = "suraj";
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log( valueInNumber);
-*/
+// console.log( valueInNumber);
 
 // "33" => 33
 // "33abcd" => NaN
 // true => 1;, false = 0;
  
-/*let isLoggedIn =0;
+let isLoggedIn =0;
 
  let booleanIsLoggedIn = Boolean(isLoggedIn);
- console.log(booleanIsLoggedIn);
-*/
+//  console.log(booleanIsLoggedIn);
 
 //  1 => true; 0 => false;
 // "" => false
@@ -24,4 +22,29 @@ console.log( valueInNumber);
 let someNumber = 33; 
 
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+
+//  ************************************ operations ***********************************************
+
+let value =+3; 
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "pacer ";
+let str2 = "suraj";
+let str3 = str1+str2;
+// console.log(str3);
+
+// console.log("1" +2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(2+4*4/3); not use this method 
+
+// console.log(+true);
+
+let gameCounter =100;
+gameCounter++;
+console.log(gameCounter);
