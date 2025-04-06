537613458899cd9ba8cad8c5252a67ec06293331
suraj tomar <140705055+pacersuraj12@users.noreply.github.com>
2025-04-06 15:31:04 +0000

    conversion of operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+/*let score = "suraj";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log( valueInNumber);
+*/
+
+// "33" => 33
+// "33abcd" => NaN
+// true => 1;, false = 0;
+ 
+/*let isLoggedIn =0;
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn);
+ console.log(booleanIsLoggedIn);
+*/
+
+//  1 => true; 0 => false;
+// "" => false
+// "suraj" => true
+
+let someNumber = 33; 
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
