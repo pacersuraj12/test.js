71329a6d3b495f7a1d401b9dc4cafab093112fef
suraj tomar <140705055+pacersuraj12@users.noreply.github.com>
2025-05-27 08:27:23 +0000

    03_scopes.js

---
+++ 03_basics/03_scopes.js
@@ -0,0 +1,36 @@
+function one(){
+    const username = "suraj";
+    function two(){
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website);
+     two()
+}
+// one()
+
+if(true){
+    const username = "suraj";
+    if(username === "suraj")
+    {
+        const website = "youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// **************************  intersesting  ************************************
+console.log(addone(5));
+
+function addone(num){
+    return num + 1;
+}
+
+
+// addtwo(4)
+const addtwo = function(num){
+    return num + 2
+}
+
+
