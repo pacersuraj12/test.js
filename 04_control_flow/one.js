7291d608108f035f4fc795acda008e1e8ba5121a
suraj tomar <140705055+pacersuraj12@users.noreply.github.com>
2025-05-27 12:06:44 +0000

    one.js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,46 @@
+// if
+
+// temperature = 41;
+
+// if(temperature===41){
+//     console.log("temperature less then 55");
+// }
+// console.log("temperature is greater than 50 ");
+
+
+// <,>, <=, ==, !=, ===, !==
+
+// const score = 200;
+// if(score >100){
+// const power = "fly"
+// console.log(`user power: ${power}`);
+// }
+// console.log(`user power: ${power}`);
+
+// const balance = 1000;
+
+// if(balance < 500){
+//     console.log("less than 500");
+// }else if(balance < 750){
+//     console.log("less than 750");
+// }else if(balance < 900){
+//     console.log("balance less then 750");
+// }else{
+//     console.log("less than 1200");
+// }
+
+ //******   real life example   ******
+
+//  const userLoggedIn = true;
+//  const debitCard = true;
+
+//  if(userLoggedIn && debitCard){
+//     console.log("Allow to buy course ");
+//  }
+
+ const loggedInFromGoogle = false;
+ const loggedInFromEmail = true;
+
+ if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("user logged in ");
+ }
