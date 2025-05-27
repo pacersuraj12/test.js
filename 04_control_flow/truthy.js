032abd9ad6ee3d22854ae8b741aefa16b5b38064
suraj tomar <140705055+pacersuraj12@users.noreply.github.com>
2025-05-27 12:53:30 +0000

    truthy.js

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,41 @@
+const userEmail = "someone@gmail.com";
+
+if(userEmail){
+    console.log("got user email");
+}else{
+    console.log("don't have user email");
+}
+
+// *******  falsy values  *********
+// false, 0, -0, 0n, "", null, undefined, naN
+
+
+// *******  truthy values  *********
+// true , "0", 'false', " ", [], {}, function(){},, 
+
+// if (userEmail.length === 0){
+//     console.log("Array is empty");
+// }
+
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("object is empty");
+}
+
+// Nullish Coalescing Operator (??): null undefined
+// let val1;
+// // val1 = 5 ?? 10;
+// // val1 = null ?? 10;
+// val1n = undefined ?? 15
+// val1 = null ?? 10 ?? 20
+
+// console.log(val1);
+
+
+// *************** ternary operator *******************
+// condition ? true : False
+
+const iceTeaPrice = 100;
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
