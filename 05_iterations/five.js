9a87b87bbf7be1c4e8162ebb2d39c02fbc713fbd
suraj tomar <140705055+pacersuraj12@users.noreply.github.com>
2025-05-28 13:27:10 +0000

    five.js

---
+++ 05_iterations/five.js
@@ -0,0 +1,13 @@
+const coding = ["js", "ruby", "java", "python", "cpp"];
+
+// coding.forEach( function (val) {
+//     console.log(val);
+// })
+
+
+coding.forEach( (item) => {
+    console.log(item);
+})
+
+
+
