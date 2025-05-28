63fa577851bb3cda664285edb78e4220d0697817
suraj tomar <140705055+pacersuraj12@users.noreply.github.com>
2025-05-28 08:18:45 +0000

    three.js

---
+++ 05_iterations/three.js
@@ -0,0 +1,28 @@
+// for of 
+
+const arr = [1,2,3,4,5];
+
+for(const num of arr){
+    // console.log(num);
+}
+
+const greetings = "hello world!";
+
+for(const greet of greetings){
+    // console.log(`each char is ${greet}`);
+}
+
+// maps
+
+// const map = new map()
+
+// map.set('In', "india")
+// map.set('usa',"united state of america ")
+// map.set('fr', "france")
+
+// console.log(map);
+
+// for(const [key, value] of map){
+//     // console.log(key, ':-', value);
+// }
+
